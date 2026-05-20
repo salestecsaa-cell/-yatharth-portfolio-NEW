e26d189f36ee725f743874afb9af3645a54bd17d
TECSAA WEBSITE'S <sales.tecsaa@gmail.com>
2026-05-20 18:25:44 +0530

    Remove Career component from MainContainer

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,6 +1,5 @@
 import { lazy, PropsWithChildren, Suspense, useEffect, useState } from "react";
 import About from "./About";
-import Career from "./Career";
 import Contact from "./Contact";
 import Cursor from "./Cursor";
 import Landing from "./Landing";
@@ -42,18 +41,17 @@ const MainContainer = ({ children }: PropsWithChildren) => {
             <Landing>{!isDesktopView && children}</Landing>
             <About />
             <WhatIDo />
-            
+
             <Suspense fallback={<div style={{ height: "600px" }}>Loading video...</div>}>
               <VideoSection />
             </Suspense>
-            
-            <Career />
+
             <Work />
-            
+
             <Suspense fallback={<div>Loading....</div>}>
               <TechStack />
             </Suspense>
-            
+
             <Contact />
           </div>
         </div>
