3f4506f0242824bd841ede73d6fcfac2e5504c14
TECSAA WEBSITE'S <sales.tecsaa@gmail.com>
2026-05-20 18:51:21 +0530

    Add phone and email icons to SocialIcons component

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -1,4 +1,4 @@
-import { FaWhatsapp } from "react-icons/fa6";
+import { FaWhatsapp, FaPhone, FaEnvelope } from "react-icons/fa6";
 import { SiUpwork } from "react-icons/si";
 import "./styles/SocialIcons.css";
 import { useEffect } from "react";
@@ -21,17 +21,14 @@ const SocialIcons = () => {
       const updatePosition = () => {
         currentX += (mouseX - currentX) * 0.1;
         currentY += (mouseY - currentY) * 0.1;
-
         link.style.setProperty("--siLeft", `${currentX}px`);
         link.style.setProperty("--siTop", `${currentY}px`);
-
         requestAnimationFrame(updatePosition);
       };
 
       const onMouseMove = (e: MouseEvent) => {
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
-
         if (x < 40 && x > 10 && y < 40 && y > 5) {
           mouseX = x;
           mouseY = y;
@@ -65,18 +62,7 @@ const SocialIcons = () => {
         </span>
         <span>
           <a href="tel:+918770794512" rel="noreferrer" title="Call">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              style={{ width: "1em", height: "1em" }}
-            >
-              <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
-            </svg>
+            <FaPhone />
           </a>
         </span>
         <span>
@@ -91,19 +77,7 @@ const SocialIcons = () => {
         </span>
         <span>
           <a href="mailto:yatharth114@gmail.com" rel="noreferrer" title="Email">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              style={{ width: "1em", height: "1em" }}
-            >
-              <rect x="2" y="4" width="20" height="16" rx="2" />
-              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
-            </svg>
+            <FaEnvelope />
           </a>
         </span>
       </div>
