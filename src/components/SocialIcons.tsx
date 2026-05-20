9eb9584a092aa60ce1c1e9faebc6c419d81bb8b4
TECSAA WEBSITE'S <sales.tecsaa@gmail.com>
2026-05-20 18:50:08 +0530

    Refactor SocialIcons component for cleaner syntax

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -1,6 +1,4 @@
-import {
-  FaWhatsapp,
-} from "react-icons/fa6";
+import { FaWhatsapp } from "react-icons/fa6";
 import { SiUpwork } from "react-icons/si";
 import "./styles/SocialIcons.css";
 import { useEffect } from "react";
@@ -44,7 +42,6 @@ const SocialIcons = () => {
       };
 
       document.addEventListener("mousemove", onMouseMove);
-
       updatePosition();
 
       return () => {
@@ -67,11 +64,7 @@ const SocialIcons = () => {
           </a>
         </span>
         <span>
-          
-            href="tel:+918770794512"
-            rel="noreferrer"
-            title="Call"
-          >
+          <a href="tel:+918770794512" rel="noreferrer" title="Call">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -82,7 +75,7 @@ const SocialIcons = () => {
               strokeLinejoin="round"
               style={{ width: "1em", height: "1em" }}
             >
-              <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+              <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
             </svg>
           </a>
         </span>
@@ -97,11 +90,7 @@ const SocialIcons = () => {
           </a>
         </span>
         <span>
-          
-            href="mailto:yatharth114@gmail.com"
-            rel="noreferrer"
-            title="Email"
-          >
+          <a href="mailto:yatharth114@gmail.com" rel="noreferrer" title="Email">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -112,8 +101,8 @@ const SocialIcons = () => {
               strokeLinejoin="round"
               style={{ width: "1em", height: "1em" }}
             >
-              <rect x="2" y="4" width="20" height="16" rx="2"></rect>
-              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+              <rect x="2" y="4" width="20" height="16" rx="2" />
+              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
             </svg>
           </a>
         </span>
